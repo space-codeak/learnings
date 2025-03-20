7573796f955c15677e048f8c4be16a0795e5e4ac
codeak_space <dev@codeak.space>
2025-03-20 12:49:01 +0530

    ff

--- src/Ios_basics/001_.playground/Contents.swift
+++ src/Ios_basics/001_.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//variables and constants
+var name="john";
+name="doem";
+print(name)
+
+let PI=3.24;
+print(PI)
+
+var city:String="india";
+print(city)
+var isRaining:Bool=false;
+print(isRaining)
+
+//operators and expressions
+
+let a=138;
+let b=38;
+let sum=a+b;
+print(sum)
+let diff=a-b;
+print(diff);
+let q=a/b;
+print(q)
+
+let r=a%b-8
+print(r)
